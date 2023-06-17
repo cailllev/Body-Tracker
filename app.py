50a3190921fb07b86013a20d95c227e30a4472f6
levi <levi.cailleret@gmail.com>
2023-06-17 16:19:55 +0200

    add routes and activities (partial)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, render_template, request, session
 from secrets import token_bytes
 
-from db import db_login, db_register, get_stats, add_stats, edit_stats, delete_stats, delete_user, categories
+from db import categories, db_init, db_login, db_register, delete_user, get_stats, add_stats, edit_stats, \
+    delete_stats, get_route_names, get_routes, add_route, edit_route, delete_route, get_activities, add_activity, \
+    edit_activity, delete_activity
 from utils import check_submission, parse_submission, beautify_stats, parse_stats_for_category, get_y_boarder, \
     headers, margin_per_category
 
@@ -9,33 +11,18 @@ app = Flask(__name__)
 app.secret_key = token_bytes(16)
 auth_user = "user"
 
+db_init()
+
 
 @app.route("/")
 def index():
-    if auth_user not in session:
-        return redirect("/login")
-
-    category = request.args.get("cat")  # TODO, frontend
-    if not category or category not in categories:
-        category = "weight"
-    category_label = category.capitalize()
-
-    stats = get_stats(session[auth_user], category)
-    round_to = 1  # round all datapoints to 1 decimal
-    datapoints, date_labels = parse_stats_for_category(stats, round_to)
-
-    margin = margin_per_category[category]
-    start_y, end_y = get_y_boarder(datapoints, margin)
-
-    return render_template("index.html", categories=categories, label=category_label,
-                           date_labels=date_labels, datapoints=datapoints, start_y=start_y, end_y=end_y)
+    return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
-
     username = request.form.get("username")
     password = request.form.get("password")
     if username and password and db_register(username, password):
@@ -48,7 +35,6 @@ def register():
 def login():
     if request.method == "GET":
         return render_template("login.html")
-
     username = request.form.get("username")
     password = request.form.get("password")
     if username and password and db_login(username, password):
@@ -63,7 +49,41 @@ def logout():
     return redirect("/")
 
 
+@app.route("/delete_user", methods=["GET", "POST"])
+def edit_entry():
+    if auth_user not in session:
+        return redirect("/login")
+    if request.method == "GET":
+        return render_template("delete_user.html")
+    delete_user(session[auth_user])
+    return render_template("index.html")
+
+
 # stats
+@app.route("/stats")
+def stats_overview():
+    if auth_user not in session:
+        return redirect("/login")
+
+    category = request.args.get("cat")  # TODO, frontend
+    if not category or category not in categories:
+        category = "weight"
+    category_label = category.capitalize()
+
+    stats = get_stats(session[auth_user], category)
+    round_to = 1  # round all datapoints to 1 decimal
+    datapoints, date_labels = parse_stats_for_category(stats, round_to)
+
+    if datapoints:
+        margin = margin_per_category[category]
+        start_y, end_y = get_y_boarder(datapoints, margin)
+    else:
+        start_y, end_y = 0, 100
+
+    return render_template("stats.html", categories=categories, label=category_label,
+                           date_labels=date_labels, datapoints=datapoints, start_y=start_y, end_y=end_y)
+
+
 @app.route("/stats/all")
 def show_all():
     if auth_user not in session:
@@ -71,24 +91,24 @@ def show_all():
 
     stats = get_stats(session[auth_user])
     beautified_stats = beautify_stats(stats)
-    return render_template("index.html", headers=headers, stats=beautified_stats)
+    return render_template("all_stats.html", categories=categories, headers=headers, stats=beautified_stats)
 
 
 @app.route("/stats/add", methods=["GET", "POST"])
 def add_entry():
     if request.method == "GET":
-        return render_template("add.html")
+        return render_template("add_stats.html")
 
     if auth_user not in session:
         return redirect("/login")
 
     ok, err = check_submission(request)
     if not ok:
-        return render_template("add.html", error=err)
+        return render_template("add_stats.html", error=err)
 
     date, weight, body_fat, water, muscles = parse_submission(request)
     add_stats(session[auth_user], date, weight, body_fat, water, muscles)
-    return redirect("/")
+    return redirect("/stats")
 
 
 @app.route("/stats/edit", methods=["GET", "POST"])
@@ -98,29 +118,29 @@ def edit_entry():
 
     ok, err = check_submission(request)  # TODO, frontend
     if not ok:
-        return render_template("add.html", error=err)  # TODO, frontend
+        return render_template("add_stats.html", error=err)  # TODO, frontend
 
     date, weight, body_fat, water, muscles = parse_submission(request)
     edit_stats(session[auth_user], date, weight, body_fat, water, muscles)
-    return render_template("index.html")  # TODO, frontend
+    return render_template("stats.html")  # TODO, frontend
 
 
 @app.route("/stats/del", methods=["GET", "POST"])
-def edit_entry():
+def del_entry():
     if auth_user not in session:
         return redirect("/login")
     date = request.form.get("date")  # TODO, frontend
     if not date:
-        return render_template("add.html", error="Date cannot be null")  # TODO, frontend
+        return render_template("add_stats.html", error="Date cannot be null")  # TODO, frontend
     delete_stats(session[auth_user], date)
     return 200
 
 
-"""
-@app.route("/delete_user", methods=["GET", "POST"])  # TODO, frontend
-def edit_entry():
-    if auth_user not in session:
-        return redirect("/login")
-    delete_user(session[auth_user])
-    return render_template("index.html")  # TODO, frontend
-"""
+# routes, TODO
+
+# activities, TODO
+
+
+# debug
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8811)
--- db.py
+++ db.py
@@ -1,12 +1,19 @@
 from hashlib import pbkdf2_hmac
 from secrets import token_bytes
 from sqlite3 import connect
+from string import ascii_letters, digits
 
 DB = "db.sqlite"
 categories = ["weight", "body_fat", "water", "muscles"]
 
 
-def init():
+def check_save_query_input(q):
+    if any(c not in ascii_letters + digits + "-_" for c in q):
+        return False  # 1337 h4xor detected
+    return True
+
+
+def db_init():
     with connect(DB) as con:
         cur = con.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS user ("
@@ -24,6 +31,24 @@ def init():
                     "PRIMARY KEY (username, date),"
                     "FOREIGN KEY (username) REFERENCES user (username) ON DELETE CASCADE"
                     ")")
+        cur.execute("CREATE TABLE IF NOT EXISTS routes ("
+                    "username TEXT,"
+                    "route_name TEXT,"
+                    "distance INT,"  # meters
+                    "height INT,"    # meters
+                    "PRIMARY KEY (username, route_name),"
+                    "FOREIGN KEY (username) REFERENCES user (username) ON DELETE CASCADE"
+                    ")")
+        cur.execute("CREATE TABLE IF NOT EXISTS activities ("
+                    "username TEXT,"
+                    "route_name TEXT,"
+                    "date INT,"      # epoch timestamp
+                    "time INT,"      # seconds
+                    "pace TEXT,"     # min/km
+                    "speed INT,"     # km/h
+                    "PRIMARY KEY (username, route_name, date),"
+                    "FOREIGN KEY (username) REFERENCES user (username) ON DELETE CASCADE"
+                    ")")
         con.commit()
 
 
@@ -38,7 +63,7 @@ def db_register(username, password):
         cur = con.cursor()
         if cur.execute("SELECT * FROM user WHERE username = (?)", (username,)).fetchall():
             return False
-        cur.execute("INSERT INTO user(username, hashed_pw, salt) "
+        cur.execute("INSERT INTO user (username, hashed_pw, salt) "
                     "VALUES (?, ?, ?)", (username, hashed_pw, salt))
         con.commit()
         return True
@@ -58,11 +83,14 @@ def db_login(username, given_password):
             return h(given_password, salt) == hashed_pw
 
 
-def get_stats(username, cat=""):
-    if not cat or cat not in categories:
+def get_stats(username, category=""):
+    if category not in categories or not check_save_query_input(category):
+        return []
+
+    if not category:
         select = "SELECT date, weight, body_fat, water, muscles"
     else:
-        select = "SELECT date, " + cat
+        select = "SELECT date, " + category
     with connect(DB) as con:
         cur = con.cursor()
         return cur.execute(select + " FROM stats WHERE username = (?) ORDER BY date", (username,)).fetchall()
@@ -71,7 +99,7 @@ def get_stats(username, cat=""):
 def add_stats(username, date, weight, body_fat, water, muscles):
     with connect(DB) as con:
         cur = con.cursor()
-        cur.execute("INSERT OR IGNORE INTO stats(username, date, weight, body_fat, water, muscles) "
+        cur.execute("INSERT OR IGNORE INTO stats (username, date, weight, body_fat, water, muscles) "
                     "VALUES (?, ?, ?, ?, ?, ?)", (username, date, weight, body_fat, water, muscles))
         con.commit()
 
@@ -89,4 +117,84 @@ def delete_stats(username, date):
     with connect(DB) as con:
         cur = con.cursor()
         cur.execute("DELETE FROM stats WHERE username = (?) and date = (?)", (username, date))
-        cur.commit()
+        con.commit()
+
+
+def get_route_names(username):
+    with connect(DB) as con:
+        cur = con.cursor()
+        return cur.execute("SELECT route_name FROM routes WHERE username = (?)", (username,)).fetchall()
+
+
+def get_routes(username, route_name=""):
+    if not check_save_query_input(route_name):
+        return []
+    if route_name:
+        add_query = f'AND route_name = "{route_name}" '
+    else:
+        add_query = "ORDER BY route_name"
+    with connect(DB) as con:
+        cur = con.cursor()
+        return cur.execute("SELECT route_name, distance, height FROM routes WHERE username = (?) " + add_query,
+                           (username,)).fetchall()
+
+
+def add_route(username, route_name, distance, height):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("INSERT OR IGNORE INTO routes (username, route_name, distance, height) VALUES (?, ?, ?, ?)",
+                    (username, route_name, distance, height))
+        con.commit()
+
+
+def edit_route(username, route_name, distance, height):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("UPDATE routes WHERE username = (?) AND route_name = (?) SET distance = (?), height = (?)",
+                    (username, route_name, distance, height))
+        con.commit()
+
+
+def delete_route(username, route_name):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM routes WHERE username = (?) and route_name = (?)", (username, route_name))
+        con.commit()
+
+
+def get_activities(username, route_name=""):
+    if not check_save_query_input(route_name):
+        return []
+    if route_name:
+        add_query = f'AND route_name = "{route_name}" ORDER by date'
+    else:
+        add_query = "ORDER BY route_name, date"
+    with connect(DB) as con:
+        cur = con.cursor()
+        return cur.execute("SELECT route_name, date, time, pace, speed FROM activities WHERE username = (?) " +
+                           add_query, (username,)).fetchall()
+
+
+def add_activity(username, route_name, date, time, pace, speed):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("INSERT OR IGNORE INTO activities (username, route_name, date, time, pace, speed) "
+                    "VALUES (?, ?, ?, ?, ?, ?)", (username, route_name, date, time, pace, speed))
+        con.commit()
+
+
+def edit_activity(username, route_name, date, time, pace, speed):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("UPDATE activities WHERE username = (?) AND route_name = (?) AND date = (?) "
+                    "SET time = (?), pace = (?), speed = (?)"
+                    , (username, route_name, date, time, pace, speed))
+        con.commit()
+
+
+def delete_activity(username, route_name, date):
+    with connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM activities WHERE username = (?) AND route_name = (?) AND date = (?)",
+                    (username, route_name, date))
+        con.commit()
